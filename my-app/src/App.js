b225fc2d6eedb6789a1bf536acf9e2a0182047ae
Alena Kharytonchyk <alena.kharytonchyk@outlook.com>
2019-12-09 20:05:05 +0300

    feat: add button and search components

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import SearchForm from "./Components/searchForm";
+import TitleText from "./Components/titleText/title";
+import SearchBy from "./Components/searchBy/search";
+
+function App() {
+  return (
+    <div className="App">
+      <TitleText/>
+      <SearchForm/>
+      <SearchBy name_One = 'TITLE' name_Two = 'GENRE' className = 'button small'/>
+    </div>
+  );
+}
+
+export default App;
