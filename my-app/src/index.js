804149965701d7137173a93ef95f0a501aadcac2
AlenaKharytonchyk <alena.kharytonchyk@outlook.com>
2019-12-13 09:12:11 +0300

    chore: replace react-scripts with webpack

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 import './index.scss';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById("root"));
